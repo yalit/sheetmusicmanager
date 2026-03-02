912f8f2b53bf1236869543d0eae16ba3727dbc78
Yannick <tech@yalit.be>
2026-03-02 21:50:36 +0100

    feat : add search and filters on Member
    - Search on name and email fields
    - TextFilter on name and email
    - ChoiceFilter on role using MemberRole enum choices

--- src/Controller/Admin/MemberCrudController.php
+++ src/Controller/Admin/MemberCrudController.php
@@ -4,15 +4,33 @@ namespace App\Controller\Admin;
 
 use App\Entity\Member;
 use App\Enum\MemberRole;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Filter\ChoiceFilter;
+use EasyCorp\Bundle\EasyAdminBundle\Filter\TextFilter;
 
 class MemberCrudController extends AbstractCrudController
 {
     public static function getEntityFqcn(): string
     {
         return Member::class;
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud->setSearchFields(['name', 'email']);
+    }
+
+    public function configureFilters(Filters $filters): Filters
+    {
+        return $filters
+            ->add(TextFilter::new('name'))
+            ->add(TextFilter::new('email'))
+            ->add(ChoiceFilter::new('role')->setChoices(MemberRole::choices()))
+        ;
     }
 
     public function configureFields(string $pageName): iterable
