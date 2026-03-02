20c356cc51b6caca8575e2374f4905851204d464
Yannick <tech@yalit.be>
2026-03-02 23:29:25 +0100

    test : update bootstrap to sync test DB schema on startup

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,6 @@
 <?php
 
+use Doctrine\ORM\Tools\SchemaTool;
 use Symfony\Component\Dotenv\Dotenv;
 
 require dirname(__DIR__).'/vendor/autoload.php';
@@ -11,3 +12,10 @@ if (method_exists(Dotenv::class, 'bootEnv')) {
 if ($_SERVER['APP_DEBUG']) {
     umask(0000);
 }
+
+// Ensure the test database schema is in sync with the current entity mapping.
+$kernel = new App\Kernel('test', true);
+$kernel->boot();
+$em = $kernel->getContainer()->get('doctrine.orm.entity_manager');
+(new SchemaTool($em))->updateSchema($em->getMetadataFactory()->getAllMetadata(), true);
+$kernel->shutdown();
