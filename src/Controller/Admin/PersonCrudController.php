52865408b3c1ed0a56fed68f36149212ec753f74
Yannick <tech@yalit.be>
2026-03-02 21:50:08 +0100

    feat : add search and filter on Person
    - Search on name field
    - TextFilter on name

--- src/Controller/Admin/PersonCrudController.php
+++ src/Controller/Admin/PersonCrudController.php
@@ -3,9 +3,12 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Person;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Filter\TextFilter;
 
 /**
  * @extends AbstractCrudController<Person>
@@ -15,6 +18,16 @@ class PersonCrudController extends AbstractCrudController
     public static function getEntityFqcn(): string
     {
         return Person::class;
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud->setSearchFields(['name']);
+    }
+
+    public function configureFilters(Filters $filters): Filters
+    {
+        return $filters->add(TextFilter::new('name'));
     }
 
     public function configureFields(string $pageName): iterable
