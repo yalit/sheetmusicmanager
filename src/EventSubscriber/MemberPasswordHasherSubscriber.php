b8d3bbd98678fe713e5ef9a8e99a178ebb83e524
Yannick <tech@yalit.be>
2026-03-02 21:05:06 +0100

    feat : hash Member password via EasyAdmin event subscriber

---
+++ src/EventSubscriber/MemberPasswordHasherSubscriber.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use App\Entity\Member;
+use EasyCorp\Bundle\EasyAdminBundle\Event\BeforeEntityPersistedEvent;
+use EasyCorp\Bundle\EasyAdminBundle\Event\BeforeEntityUpdatedEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+readonly class MemberPasswordHasherSubscriber implements EventSubscriberInterface
+{
+    public function __construct(
+        private UserPasswordHasherInterface $passwordHasher,
+    ) {}
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            BeforeEntityPersistedEvent::class => 'hashPassword',
+            BeforeEntityUpdatedEvent::class => 'hashPassword',
+        ];
+    }
+
+    public function hashPassword(BeforeEntityPersistedEvent|BeforeEntityUpdatedEvent $event): void
+    {
+        $entity = $event->getEntityInstance();
+        if (!$entity instanceof Member || !$entity->getPlainPassword()) {
+            return;
+        }
+
+        $entity->setPassword(
+            $this->passwordHasher->hashPassword($entity, $entity->getPlainPassword())
+        );
+    }
+}
