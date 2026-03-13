31a7f0fe69ecb94e5d81ae6e0e5b144e9515afc4
Yannick <tech@yalit.be>
2026-03-13 11:02:05 +0100

    test : add unit tests for SetlistItemFactory::clone()

---
+++ tests/Entity/Factory/SetlistItemFactoryTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Tests\Entity\Factory;
+
+use App\Entity\Factory\SetlistItemFactory;
+use App\Entity\SetListItem;
+use App\Entity\Sheet;
+use PHPUnit\Framework\TestCase;
+
+final class SetlistItemFactoryTest extends TestCase
+{
+    private function makeItem(): SetListItem
+    {
+        $sheet = (new Sheet())->setTitle('Sheet A');
+
+        return (new SetListItem())
+            ->setPosition(3)
+            ->setName('Intro')
+            ->setNotes('Play softly')
+            ->setSheet($sheet);
+    }
+
+    public function testCloneCopiesAllFields(): void
+    {
+        $original = $this->makeItem();
+
+        $clone = SetlistItemFactory::clone($original);
+
+        static::assertSame($original->getPosition(), $clone->getPosition());
+        static::assertSame($original->getName(), $clone->getName());
+        static::assertSame($original->getNotes(), $clone->getNotes());
+        static::assertSame($original->getSheet(), $clone->getSheet());
+    }
+
+    public function testCloneIsDistinctObject(): void
+    {
+        $original = $this->makeItem();
+
+        $clone = SetlistItemFactory::clone($original);
+
+        static::assertNotSame($original, $clone);
+    }
+}
