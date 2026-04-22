274e538a6d5f31573e542afba0200a2dbbeacae1
Yannick <tech@yalit.be>
2026-04-22 23:06:59 +0200

    ref : refactor Message and Handlers for actions

--- src/MessageHandler/DuplicateSetlistHandler.php
+++ src/MessageHandler/DuplicateSetlistHandler.php
@@ -1,9 +1,10 @@
 <?php
 
-namespace App\Admin\Action;
+namespace App\MessageHandler;
 
 use App\Entity\Setlist\Factory\SetlistFactory;
 use App\Entity\Setlist\Setlist;
+use App\Message\DuplicateSetlist;
 use App\Repository\SetlistRepository;
 
 final readonly class DuplicateSetlistHandler
